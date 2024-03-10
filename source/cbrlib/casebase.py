3c7e49a3f6cc12209bb1f00b49fa9a1560f248dc
Christian Dein <christian.dein@dein-hosting.de>
2024-03-10 18:33:00 +0100

    Calculated facets including entropy scoring

--- source/cbrlib/casebase.py
+++ source/cbrlib/casebase.py
@@ -1,39 +1,28 @@
-from dataclasses import dataclass
-import dataclasses
-from typing import Generic, Iterable, TypeVar
+from typing import Any, Callable, Iterable, Optional
 
 from cbrlib.evaluate import Evaluator
+from cbrlib.facetting import FacetCollectingIterator
+from cbrlib.types import C, Facet, ReasoningRequest, ReasoningResponse, Result
 
 
-C = TypeVar("C")
-
-
-@dataclass(slots=True, frozen=True)
-class Result(Generic[C]):
-    similarity: float
-    case: C
-
-
-@dataclass(slots=True, frozen=True)
-class ReasoningRequest(Generic[C]):
-    query: C
-
-    offset: int = dataclasses.field(default=0)
-    limit: int = dataclasses.field(default=10)
-
-    threshold: float = dataclasses.field(default=0.1)
-
-
-@dataclass(slots=True, frozen=True)
-class ReasoningResponse(Generic[C]):
-    total_number_of_hits: int
-    hits: Iterable[Result]
+def _make_relevant_facets(
+    query: Any, facets: Iterable[Facet], getvalue: Callable[[Any, str, Optional[Any]], Any]
+) -> list[Facet]:
+    facet_names = set(f.name for f in facets)
+    return list(
+        filter(
+            lambda f: f.name in facet_names and getvalue(query, f.name, None) is not None,
+            facets,
+        )
+    )
 
 
 def infer(
     casebase: Iterable[C],
     request: ReasoningRequest[C],
     evaluator: Evaluator,
+    *,
+    getvalue: Callable[[Any, str, Optional[Any]], Any] = getattr,
 ) -> ReasoningResponse[C]:
 
     threshold = request.threshold
@@ -46,16 +35,21 @@ def infer(
         key=lambda r: r.similarity,
         reverse=True,
     )
-    filtered_results = filter(
+    calculate_results = filter(
         lambda r: r.similarity >= threshold,
         sorted_results,
     )
-    result_list = list(filtered_results)
+    if request.facets is not None:
+        relevant_facets = _make_relevant_facets(request.query, request.facets, getvalue)
+        calculate_results = FacetCollectingIterator(calculate_results, relevant_facets, getvalue=getvalue)
+    result_list = list(calculate_results)
     total_number_of_hits = len(result_list)
 
     offset = request.offset
     limit = request.limit
+    result_list = result_list[offset:offset + limit]  # fmt: skip
     return ReasoningResponse(
         total_number_of_hits,
-        result_list[offset:offset + limit],  # fmt: skip
+        hits=result_list,
+        facets=calculate_results.facets if request.facets is not None else None,
     )
--- tests/test_casebase_infer.py
+++ tests/test_casebase_infer.py
@@ -4,6 +4,7 @@ from typing import Optional
 
 from cbrlib import casebase, evaluate
 from cbrlib.casebase import ReasoningRequest, ReasoningResponse
+from cbrlib.types import FacetConfig
 
 
 @dataclass
@@ -64,3 +65,51 @@ def test_evaluate_casebase_similarity() -> None:
     assert response.total_number_of_hits == 2
     assert response.hits[0].similarity == 1
     assert response.hits[1].similarity == 0.8
+
+
+def test_evaluate_casebase_facets() -> None:
+    request = ReasoningRequest(
+        query=DataObject(color="red"),
+        threshold=0,
+        facets=(
+            FacetConfig("color", 4),
+            FacetConfig("irrelevant", 4),
+        ),
+    )
+    response = casebase.infer(
+        [
+            DataObject(color="red"),
+            DataObject(color="red"),
+            DataObject(color="red"),
+            DataObject(color="red"),
+            DataObject(color="red"),
+            DataObject(color="green"),
+            DataObject(color="green"),
+            DataObject(color="green"),
+            DataObject(color="green"),
+            DataObject(color="yellow"),
+            DataObject(color="yellow"),
+            DataObject(color="yellow"),
+            DataObject(color="orange"),
+            DataObject(color="orange"),
+            DataObject(color="blue"),
+            DataObject(color="purple"),
+            DataObject(color="magenta"),
+            DataObject(),  # Allow also empty objects
+            DataObject(color=None),  # or missing property value
+        ],
+        request,
+        dataobject_equality_evaluator,
+    )
+    assert isinstance(response, ReasoningResponse)
+    assert response.facets is not None
+    assert len(response.facets) == 1
+    assert len(response.facets[0].values) == 4
+    assert response.facets[0].values[0].value == "red"
+    assert response.facets[0].values[0].count == 5
+    assert response.facets[0].values[1].value == "green"
+    assert response.facets[0].values[1].count == 4
+    assert response.facets[0].values[2].value == "yellow"
+    assert response.facets[0].values[2].count == 3
+    assert response.facets[0].values[3].value == "orange"
+    assert response.facets[0].values[3].count == 2
